5ca20972523f2d3e115da083f0cc5a8a05b92098
Noha <nohaa.mdj@gmail.com>
2025-02-18 20:25:59 +0100

    réglages filtres

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,19 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
+$(document).ready(function () {
+  $(".gallery").mauGallery({
+    columns: {
+      xs: 1,
+      sm: 2,
+      md: 3,
+      lg: 3,
+      xl: 3,
+    },
+    lightBox: true,
+    lightboxId: "myAwesomeLightbox",
+    showTags: true,
+    tagsPosition: "top",
+  });
+  $(".nav-pills .nav-link").click(function () {
+    $(".nav-pills .nav-link").removeClass("active"); // Supprime l'effet actif sur tous les boutons
+    $(this).addClass("active"); // Ajoute l'effet actif sur le bouton sélectionné
+  });
 });
